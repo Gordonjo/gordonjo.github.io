e5ed11eb39e6aa03ef17b03248fce57e3097030d
gordonjo <gordonjo@openai.com>
2022-12-25 11:57:21 -0800

    Adding GA to site

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import ReactGA from 'react-ga'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
@@ -11,6 +12,14 @@ import Footer from './components/footer/Footer'
 
 
 const App = () => {
+
+  ReactGA.initialize('UA-180159529-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+  
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [window.location]);
+
   return (
     <div>
         <Header />
